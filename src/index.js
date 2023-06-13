d98617a05e9dff1b56208ed93cde64b4fb7675f9
Edison611 <edisony611@gmail.com>
2023-06-13 19:39:25 -0400

    feat : hash routing test

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter as Router } from 'react-router-dom'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
+    <Router>
       <App />
-    </BrowserRouter>
-  </React.StrictMode>
+    </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
