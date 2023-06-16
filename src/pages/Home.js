cf851a8e46ec38516b11259843288e7081f321c2
Edison611 <edisony611@gmail.com>
2023-06-16 13:09:13 -0400

    feat : homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,20 @@
 import React from "react";
 import "../styles/pages.css";
+import background from "../images/background2.jpeg"
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+    const navigate = useNavigate();
     return (
-        <div>
-            <h1> Under Construction </h1>
-        </div>
+        <div className="container"> 
+            <img src={background} alt="" className="back">
+            </img>
+            <div className="hello"> Hi, </div>
+            <div className="me">I'm Edison Ying</div>
+            <div className="description"> I am a programmer coming into the industry and this is my portfolio</div>
+            <div className="beap">You can find my coursework in the ICS4U0 course here: </div>
+            <button className="button-42" onClick={ () => navigate("/ICS4U0")} >ICS4U0</button>
+       </div>
     )
 }
 
